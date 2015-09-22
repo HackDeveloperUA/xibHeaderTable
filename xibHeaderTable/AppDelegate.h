c85d478c61a2afe78b12158ec9d4f21c914fc718
MD <md@MacBook-Air-MD.Dlink>
2015-09-22 20:37:02 +0300

    Initial Commit

---
+++ xibHeaderTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xibHeaderTable
+//
+//  Created by MD on 22.09.15.
+//  Copyright (c) 2015 MD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xibHeaderTable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xibHeaderTable
+//
+//  Created by MD on 22.09.15.
+//  Copyright (c) 2015 MD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
